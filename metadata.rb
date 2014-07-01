5408579d5181d5964f1d4111ff68a3794f60b672
Jose Lopez <jose.lopez@sendgrid.com>
2014-06-30 19:57:47 -0700

    minor tweaks to berksfile

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,6 @@ description      'Install and configure host machine'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1'
 
-recipe            "boostrap::default", "Default bootstrap option"
+recipe            "bootstrap::default", "Default bootstrap option"
 
 depends "apt"
