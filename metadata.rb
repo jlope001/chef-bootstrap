9fd2b72c351ba4de5cc3b144c2bdd68499a44c0b
Jose Lopez <jose.lopez@sendgrid.com>
2014-06-30 17:16:13 -0700

    initial push for default system packages

--- metadata.rb
+++ metadata.rb
@@ -6,11 +6,6 @@ description      'Install and configure host machine'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1'
 
-recipe            "boostrap", "All current development environments"
-recipe            "development::base", "Install base system components"
-recipe            "development::bootstrap", "Bootstrap the vm with base cookbooks"
-recipe            "development::lamp", "Installs LAMP stack"
-recipe            "development::site", "Installs sendgrid symfony site"
-recipe            "development::cam", "Installs cam app to site"
+recipe            "boostrap::default", "Default bootstrap option"
 
 depends "apt"
---
+++ recipes/packages.rb
@@ -0,0 +1,52 @@
+# install base packages
+%w{curl git vim chromium-browser keepassx guake rdiff-backup}.each do |pkg|
+  package pkg do
+    action :install
+  end
+end
+
+# system tray software
+%w{indicator-multiload hamster-indicator indicator-cpufreq}.each do |pkg|
+  package pkg do
+    action :install
+  end
+end
+
+# vm software
+%w{nfs-common nfs-kernel-server}.each do |pkg|
+  package pkg do
+    action :install
+  end
+end
+
+# vpn software
+%w{libnss-myhostname openvpn network-manager-openvpn network-manager-openvpn-gnome}.each do |pkg|
+  package pkg do
+    action :install
+  end
+end
+
+# multimedia updates
+%w{ubuntu-restricted-extras libxslt-dev libxml2-dev libdvdread4}.each do |pkg|
+  package pkg do
+    action :install
+  end
+end
+
+# uninstall useless packages
+%w{unity-lens-shopping}.each do |pkg|
+  package pkg do
+    action :install
+  end
+end
+
+
+# libsendgrid setup
+execute "enable dvd playback" do
+  command "/usr/share/doc/libdvdread4/install-css.sh"
+  action :run
+end
+execute "disable privacy settings" do
+  command "gsettings set com.canonical.Unity.Lenses disabled-scopes \"['more_suggestions-amazon.scope', 'more_suggestions-u1ms.scope', 'more_suggestions-populartracks.scope', 'music-musicstore.scope', 'more_suggestions-ebay.scope', 'more_suggestions-ubuntushop.scope', 'more_suggestions-skimlinks.scope']\""
+  action :run
+end
