8668296429b82f840402d8df68bb7f839f5d43db
Jose Lopez <jose.lopez@sendgrid.com>
2014-07-05 10:45:14 -0700

    add auto accept java license agreement

--- site-cookbooks/bootstrap/recipes/java.rb
+++ site-cookbooks/bootstrap/recipes/java.rb
@@ -5,6 +5,17 @@ apt_repository 'java' do
   keyserver    'keyserver.ubuntu.com'
   key          'EEA14886'
 end
+
+# show that you've read the license agreement
+execute "java license agreement auto select true" do
+  command "echo debconf shared/accepted-oracle-license-v1-1 select true | debconf-set-selections"
+  action :run
+end
+execute "java license agreement auto select true" do
+  command "echo debconf shared/accepted-oracle-license-v1-1 seen true | debconf-set-selections"
+  action :run
+end
+
 package "oracle-java7-installer" do
   action :install
 end
