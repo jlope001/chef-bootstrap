f1275bf79e1e49d1e79da54616fb1ec671314543
Jose Lopez <jose.lopez@sendgrid.com>
2014-07-04 20:53:53 -0700

    moved packages to something a bit better like base

--- site-cookbooks/bootstrap/recipes/base.rb
+++ site-cookbooks/bootstrap/recipes/base.rb
@@ -1,12 +1,5 @@
 # install base packages
-%w{curl git vim chromium-browser keepassx guake rdiff-backup}.each do |pkg|
-  package pkg do
-    action :install
-  end
-end
-
-# system tray software
-%w{indicator-multiload hamster-indicator indicator-cpufreq}.each do |pkg|
+%w{curl git vim chromium-browser keepassx rdiff-backup}.each do |pkg|
   package pkg do
     action :install
   end
@@ -47,18 +40,3 @@ execute "disable privacy settings" do
   command "gsettings set com.canonical.Unity.Lenses disabled-scopes \"['more_suggestions-amazon.scope', 'more_suggestions-u1ms.scope', 'more_suggestions-populartracks.scope', 'music-musicstore.scope', 'more_suggestions-ebay.scope', 'more_suggestions-ubuntushop.scope', 'more_suggestions-skimlinks.scope']\""
   action :run
 end
-
-
-# gnomefs
-apt_repository 'gencfsm' do
-  keyserver    'keyserver.ubuntu.com'
-  uri          'http://ppa.launchpad.net/gencfsm/ppa/ubuntu'
-  distribution distribution  node[:bootstrap][:distribution]
-  components   ['main']
-  keyserver    'keyserver.ubuntu.com'
-  key          '0F68ADCA'
-end
-package "gnome-encfs-manager" do
-  action :install
-end
-
