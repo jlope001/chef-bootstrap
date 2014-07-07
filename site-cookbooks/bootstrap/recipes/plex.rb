5db14a0bc5efd2886a37cae5f985c3d2b328b5bc
Jose Lopez <jose.lopez@sendgrid.com>
2014-07-07 15:17:27 -0700

    updates to plex media player

--- site-cookbooks/bootstrap/recipes/plex.rb
+++ site-cookbooks/bootstrap/recipes/plex.rb
@@ -1,15 +1,17 @@
-# install dependency
-package "avahi-daemon" do
-  action :install
-end
-
-apt_repository 'plexmediaserver' do
-  uri          'http://www.plexapp.com/repo'
-  distribution distribution  node[:bootstrap][:distribution]
+apt_repository 'plex' do
+  uri          'http://ppa.launchpad.net/plexapp/plexht/ubuntu'
+  distribution  node[:bootstrap][:distribution]
   components   ['main']
-  # key          'https://www.hipchat.com/keys/hipchat-linux.key'
+  keyserver    'keyserver.ubuntu.com'
+  key          'EB7DFFFB'
 end
-
 package "plexmediaserver" do
   action :install
+end
+
+# add newly created plux user to user group
+group node[:bootstrap] do
+  action :modify
+  members "plex"
+  append true
 end
