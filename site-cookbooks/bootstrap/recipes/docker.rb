764696d419551d654f9911667c54850efbbf775c
Jose Lopez <jose.lopez@sendgrid.com>
2014-07-07 15:32:36 -0700

    forgot to include keyserver

--- site-cookbooks/bootstrap/recipes/docker.rb
+++ site-cookbooks/bootstrap/recipes/docker.rb
@@ -2,6 +2,7 @@ apt_repository 'docker' do
   uri          'https://get.docker.io/ubuntu'
   distribution 'docker'
   components   ['main']
+  keyserver    'keyserver.ubuntu.com'
   key          '36A1D7869245C8950F966E92D8576A8BA88D21E9'
 end
 
