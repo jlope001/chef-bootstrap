cd6b311b79d2cb751cda2c803a109075deb7c005
Jose Lopez <jose.lopez@sendgrid.com>
2014-07-07 19:21:48 -0700

    fix plex media server setup issue

--- site-cookbooks/bootstrap/recipes/plex.rb
+++ site-cookbooks/bootstrap/recipes/plex.rb
@@ -1,16 +1,19 @@
-# install plex media server
-version = "0.9.9.12.504-3e7f93c"
-deb_filename = "plexmediaserver_#{version}_amd64.deb"
-remote_file "/tmp/#{deb_filename}" do
-  source "http://downloads.plexapp.com/plex-media-server/#{version}/#{deb_filename}"
-  mode 0644
-end
-
-dpkg_package "plexmediaserver" do
-  source "/tmp/#{deb_filename}"
+# install dependency
+package "avahi-daemon" do
   action :install
 end
 
-file "/tmp/#{deb_filename}" do
-  action :delete
+apt_repository 'plex' do
+  uri          'http://plex.r.worldssl.net/PlexMediaServer/ubuntu-repo'
+  distribution 'lucid'
+  components   ['main']
+end
+
+package "plexmediaserver" do
+  action :install
+end
+
+# after the install - the package installs its own source, remove default
+apt_repository 'plex' do
+  action :remove
 end
