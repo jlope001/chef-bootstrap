198f90df6bbd3c7260053f62e1d298ada803679e
Jose Lopez <jose.lopez@sendgrid.com>
2014-07-12 13:22:48 -0700

    add additional recipe options

--- site-cookbooks/bootstrap/metadata.rb
+++ site-cookbooks/bootstrap/metadata.rb
@@ -7,6 +7,8 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1'
 
 recipe            "bootstrap::default", "Default bootstrap option"
+recipe            "bootstrap::plex", "Include plex media server"
+recipe            "bootstrap::docker", "Install docker system"
 
 depends "apt"
 depends "docker"
