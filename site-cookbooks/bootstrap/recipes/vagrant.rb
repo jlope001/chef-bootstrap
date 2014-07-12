a86857034541b8270f5fd1506c2f282638567fbe
Jose Lopez <jose.lopez@sendgrid.com>
2014-07-12 13:23:24 -0700

    revert default recipe and add centos capabilities for vagrant

--- site-cookbooks/bootstrap/recipes/vagrant.rb
+++ site-cookbooks/bootstrap/recipes/vagrant.rb
@@ -1,19 +1,43 @@
-# install vagrant
-vagrant_deb_filename = "vagrant_1.6.3_x86_64.deb"
-remote_file "/tmp/#{vagrant_deb_filename}" do
-  source "https://dl.bintray.com/mitchellh/vagrant/#{vagrant_deb_filename}"
-  mode 0644
-  checksum "0fc3259cf08b693e3383636256734513ee93bf258f8328efb64e1dde447aadbe"
+case node['platform']
+
+# install ubuntu vagrant
+when 'ubuntu'
+  # install vagrant
+  vagrant_deb_filename = "vagrant_1.6.3_x86_64.deb"
+  remote_file "/tmp/#{vagrant_deb_filename}" do
+    source "https://dl.bintray.com/mitchellh/vagrant/#{vagrant_deb_filename}"
+    mode 0644
+    checksum "0fc3259cf08b693e3383636256734513ee93bf258f8328efb64e1dde447aadbe"
+  end
+
+  dpkg_package "vagrant" do
+    source "/tmp/#{vagrant_deb_filename}"
+    action :install
+  end
+
+  file "/tmp/#{vagrant_deb_filename}" do
+    action :delete
+  end
+
+# install centos
+when 'centos'
+  vagrant_rpm_filename = "vagrant_1.6.3_x86_64.rpm"
+  remote_file "/tmp/#{vagrant_rpm_filename}" do
+    source "https://dl.bintray.com/mitchellh/vagrant/#{vagrant_rpm_filename}"
+    mode 0644
+    checksum "0fc3259cf08b693e3383636256734513ee93bf258f8328efb64e1dde447aadbe"
+  end
+
+  rpm_package "vagrant" do
+    action :install
+    source "/tmp/#{vagrant_rpm_filename}"
+  end
+
+  file "/tmp/#{vagrant_rpm_filename}" do
+    action :delete
+  end
 end
 
-dpkg_package "vagrant" do
-  source "/tmp/#{vagrant_deb_filename}"
-  action :install
-end
-
-file "/tmp/#{vagrant_deb_filename}" do
-  action :delete
-end
 
 # install vagrant berkshelf
 execute "install vagrant berkshelf plugin" do
