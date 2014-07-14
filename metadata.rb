1e3d59ab3685c28d5e296fdee803eb5e9c187d86
Jose Lopez <jose.lopez@sendgrid.com>
2014-07-13 21:25:41 -0700

    update bootstrap recipe allowed list and version bump

--- metadata.rb
+++ metadata.rb
@@ -4,11 +4,13 @@ maintainer_email 'jlope001@gmail.com'
 license          'All rights reserved'
 description      'Install and configure host machine'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.1'
+version          '0.1.2'
 
 recipe            "bootstrap::default", "default bootstrap option"
-recipe            "bootstrap::plex", "include plex media server"
-recipe            "bootstrap::docker", "install docker system"
+recipe            "bootstrap::plex", "steam movies via plex media server"
+recipe            "bootstrap::vagrant", "vm manager"
+recipe            "bootstrap::docker", "docker system"
+recipe            "bootstrap::zsh", "terminal setup"
 
 depends "apt"
 depends "docker"
