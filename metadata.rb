a4b9375b395b58909a8ec0f5ea17e33a47d8d0d7
Jose Lopez <jose.lopez@sendgrid.com>
2014-07-13 20:02:00 -0700

    tweak metadata information

--- metadata.rb
+++ metadata.rb
@@ -6,9 +6,9 @@ description      'Install and configure host machine'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1'
 
-recipe            "bootstrap::default", "Default bootstrap option"
-recipe            "bootstrap::plex", "Include plex media server"
-recipe            "bootstrap::docker", "Install docker system"
+recipe            "bootstrap::default", "default bootstrap option"
+recipe            "bootstrap::plex", "include plex media server"
+recipe            "bootstrap::docker", "install docker system"
 
 depends "apt"
 depends "docker"
