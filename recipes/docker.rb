8037e61611fcb4abd061a4bc63ca4ff919731012
Jose Lopez <jose.lopez@sendgrid.com>
2014-07-21 15:53:20 -0700

    update docker to add docker group and include user into that group, fix vagrant setup and version bump

--- recipes/docker.rb
+++ recipes/docker.rb
@@ -1,1 +1,15 @@
 include_recipe "docker"
+
+# create and add USER into docker group so we dont require sudo all the time
+group "docker" do
+  action :create
+end
+group "docker" do
+  action :modify
+  members node[:bootstrap][:user]
+  append true
+end
+service "docker" do
+  action :restart
+end
+
--- recipes/vagrant.rb
+++ recipes/vagrant.rb
@@ -41,7 +41,7 @@ end
 
 # install vagrant berkshelf
 execute "install vagrant berkshelf plugin" do
-  command "vagrant plugin install vagrant-berkshelf --plugin-version '>= 2.0.1'"
+  command "NOKOGIRI_USE_SYSTEM_LIBRARIES=1; vagrant plugin install --verbose vagrant-berkshelf --plugin-version '>= 2.0.1'"
   action  :run
   user    node[:bootstrap][:user]
   group   node[:bootstrap][:user]
