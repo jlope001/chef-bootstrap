07831e8a612f0735ff718bb1d658e47a0997be6d
Jose Lopez <jose.lopez@sendgrid.com>
2014-09-23 09:03:39 -0700

    update cookbooks and add version to docker

--- attributes/attrubutes.rb
+++ attributes/attrubutes.rb
@@ -2,3 +2,6 @@ default[:bootstrap][:user] = "#{ENV['USER']}"
 
 # default ubuntu distribution
 default[:bootstrap][:distribution] = "trusty"
+
+# default docker distro
+default[:docker][:version] = "latest"
