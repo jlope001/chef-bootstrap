aaf5d7055fba51f6da9ff158e948d46b9c456ddc
Jose Lopez <jose.lopez@sendgrid.com>
2014-09-25 11:01:02 -0700

    more configuration changes for centos7

--- recipes/epel.rb
+++ recipes/epel.rb
@@ -1,49 +1,56 @@
+is_centos_6 = false
+is_centos_7 = false
+
 # configure all required EPEL repositories
 case node['platform']
 when 'centos'
 
   ruby_block "determine centOS version" do
       block do
-        #tricky way to load this Chef::Mixin::ShellOut utilities
+        # tricky way to load this Chef::Mixin::ShellOut utilities
         Chef::Resource::RubyBlock.send(:include, Chef::Mixin::ShellOut)
         curl_command = "cat /etc/centos-release | grep 'release 6'"
-        curl_command_out = shell_out(curl_command)
+        is_centos_6 = shell_out(curl_command)
 
-        # centOS 6
-        if curl_command_out
+        curl_command = "cat /etc/centos-release | grep 'release 7'"
+        is_centos_7 = shell_out(curl_command)
 
-          # EPEL
-          yum_repository 'epel' do
-            mirrorlist 'http://mirrors.fedoraproject.org/mirrorlist?repo=epel-6&arch=$basearch'
-            description 'Extra Packages for Enterprise Linux 6 - $basearch'
-            enabled true
-            gpgcheck true
-            gpgkey 'http://dl.fedoraproject.org/pub/epel/RPM-GPG-KEY-EPEL'
-          end
-
-        # centOS 7
-        else
-
-          # EPEL
-          yum_repository 'epel' do
-            mirrorlist 'http://mirrors.fedoraproject.org/mirrorlist?repo=epel-7&arch=$basearch'
-            description 'Extra Packages for Enterprise Linux 7 - $basearch'
-            enabled true
-            gpgcheck true
-            gpgkey 'http://dl.fedoraproject.org/pub/epel/RPM-GPG-KEY-EPEL'
-          end
-
-          # docker
-          remote_file "#{Chef::Config[:file_cache_path]}/goldmann-docker-io-epel-7.repo" do
-              source "https://copr.fedoraproject.org/coprs/goldmann/docker-io/repo/epel-7/goldmann-docker-io-epel-7.repo"
-              action :create
-          end
-          rpm_package "docker" do
-              source "#{Chef::Config[:file_cache_path]}/goldmann-docker-io-epel-7.repo"
-              action :install
-          end
-        end
       end
       action :create
+  end
+
+  # centOS 6
+  if is_centos_6
+
+    # EPEL
+    yum_repository 'epel' do
+      mirrorlist 'http://mirrors.fedoraproject.org/mirrorlist?repo=epel-6&arch=$basearch'
+      description 'Extra Packages for Enterprise Linux 6 - $basearch'
+      enabled true
+      gpgcheck true
+      gpgkey 'http://dl.fedoraproject.org/pub/epel/RPM-GPG-KEY-EPEL'
+    end
+
+  # centOS 7
+  elsif is_centos_7
+
+    # EPEL
+    yum_repository 'epel' do
+      mirrorlist 'http://mirrors.fedoraproject.org/mirrorlist?repo=epel-7&arch=$basearch'
+      description 'Extra Packages for Enterprise Linux 7 - $basearch'
+      enabled true
+      gpgcheck true
+      gpgkey 'http://dl.fedoraproject.org/pub/epel/RPM-GPG-KEY-EPEL'
+    end
+
+    # docker
+    remote_file "#{Chef::Config[:file_cache_path]}/goldmann-docker-io-epel-7.repo" do
+        source "https://copr.fedoraproject.org/coprs/goldmann/docker-io/repo/epel-7/goldmann-docker-io-epel-7.repo"
+        action :create
+    end
+    rpm_package "docker" do
+        source "#{Chef::Config[:file_cache_path]}/goldmann-docker-io-epel-7.repo"
+        action :install
+    end
   end
 end
