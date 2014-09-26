4366def7ba17822a139a6de0c489a98d0f9291c9
Jose Lopez <jose.lopez@sendgrid.com>
2014-09-25 22:00:23 -0700

    tweak centos7 docker setup

--- recipes/epel.rb
+++ recipes/epel.rb
@@ -4,23 +4,20 @@ is_centos_7 = false
 # configure all required EPEL repositories
 case node['platform']
 when 'centos'
+  find = Mixlib::ShellOut.new("cat /etc/centos-release | grep 'release 6'")
+  find.run_command
+  is_centos_6 = find.stdout
 
-  ruby_block "determine centOS version" do
-      block do
-        # tricky way to load this Chef::Mixin::ShellOut utilities
-        Chef::Resource::RubyBlock.send(:include, Chef::Mixin::ShellOut)
-        curl_command = "cat /etc/centos-release | grep 'release 6'"
-        is_centos_6 = shell_out(curl_command)
-
-        curl_command = "cat /etc/centos-release | grep 'release 7'"
-        is_centos_7 = shell_out(curl_command)
-
-      end
-      action :create
-  end
+  find = Mixlib::ShellOut.new("cat /etc/centos-release | grep 'release 7'")
+  find.run_command
+  is_centos_7 = find.stdout
 
   # centOS 6
-  if is_centos_6
+  if ! is_centos_6.empty?
+    execute "centos 6 detected" do
+      command "echo 'hello 6'"
+      action :run
+    end
 
     # EPEL
     yum_repository 'epel' do
@@ -32,7 +29,11 @@ when 'centos'
     end
 
   # centOS 7
-  elsif is_centos_7
+  elsif ! is_centos_7.empty?
+    execute "centos7 detected" do
+      command "echo 'hello 7'"
+      action :run
+    end
 
     # EPEL
     yum_repository 'epel' do
@@ -42,15 +43,9 @@ when 'centos'
       gpgcheck true
       gpgkey 'http://dl.fedoraproject.org/pub/epel/RPM-GPG-KEY-EPEL'
     end
-
-    # docker
-    remote_file "#{Chef::Config[:file_cache_path]}/goldmann-docker-io-epel-7.repo" do
+    remote_file "/etc/yum.repos.d/docker.repo" do
         source "https://copr.fedoraproject.org/coprs/goldmann/docker-io/repo/epel-7/goldmann-docker-io-epel-7.repo"
         action :create
-    end
-    rpm_package "docker" do
-        source "#{Chef::Config[:file_cache_path]}/goldmann-docker-io-epel-7.repo"
-        action :install
     end
   end
 end
