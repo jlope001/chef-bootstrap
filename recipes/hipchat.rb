146763725e9e69568439a6bcc6de749475a22ffd
Jose Lopez <jose.lopez@sendgrid.com>
2015-06-01 14:23:59 -0700

    fix syntax errors

--- recipes/hipchat.rb
+++ recipes/hipchat.rb
@@ -10,14 +10,14 @@ package "hipchat" do
 end
 
 [
-  'wget http://raelcunha.com/ubuntu-mono-dark/hipchat.svg -O /usr/share/icons/ubuntu-mono-dark/apps/22/hipchat.svg'
-  'wget http://raelcunha.com/ubuntu-mono-light/hipchat.svg -O /usr/share/icons/ubuntu-mono-light/apps/22/hipchat.svg'
-  'wget http://raelcunha.com/ubuntu-mono-dark/hipchat-attention.svg -O /usr/share/icons/ubuntu-mono-dark/apps/22/hipchat-attention.svg'
-  'wget http://raelcunha.com/ubuntu-mono-dark/48.svg -O /usr/share/icons/ubuntu-mono-dark/apps/48/hipchat.svg'
-  'ln -sf /usr/share/icons/ubuntu-mono-dark/apps/22/hipchat-attention.svg /usr/share/icons/ubuntu-mono-light/apps/22/hipchat-attention.svg'
-  'ln -sf /usr/share/icons/ubuntu-mono-dark/apps/48/hipchat.svg /usr/share/icons/ubuntu-mono-light/apps/48/hipchat.sv'
-  'gtk-update-icon-cache --ignore-theme-index -f /usr/share/icons/ubuntu-mono-dark'
-  'gtk-update-icon-cache --ignore-theme-index -f /usr/share/icons/ubuntu-mono-light    '
+  'wget http://raelcunha.com/ubuntu-mono-dark/hipchat.svg -O /usr/share/icons/ubuntu-mono-dark/apps/22/hipchat.svg',
+  'wget http://raelcunha.com/ubuntu-mono-light/hipchat.svg -O /usr/share/icons/ubuntu-mono-light/apps/22/hipchat.svg',
+  'wget http://raelcunha.com/ubuntu-mono-dark/hipchat-attention.svg -O /usr/share/icons/ubuntu-mono-dark/apps/22/hipchat-attention.svg',
+  'wget http://raelcunha.com/ubuntu-mono-dark/48.svg -O /usr/share/icons/ubuntu-mono-dark/apps/48/hipchat.svg',
+  'ln -sf /usr/share/icons/ubuntu-mono-dark/apps/22/hipchat-attention.svg /usr/share/icons/ubuntu-mono-light/apps/22/hipchat-attention.svg',
+  'ln -sf /usr/share/icons/ubuntu-mono-dark/apps/48/hipchat.svg /usr/share/icons/ubuntu-mono-light/apps/48/hipchat.sv',
+  'gtk-update-icon-cache --ignore-theme-index -f /usr/share/icons/ubuntu-mono-dark',
+  'gtk-update-icon-cache --ignore-theme-index -f /usr/share/icons/ubuntu-mono-light',
 ].each do |command_to_run|
   execute "update hipchat icons to be monochrome" do
     command command_to_run
