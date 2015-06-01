da98549ae8e3f45b6cf93534256fcf41aee40ac0
Jose Lopez <jose.lopez@sendgrid.com>
2015-06-01 16:29:03 -0700

    initial push for golang setup with goimports

--- recipes/golang.rb
+++ recipes/golang.rb
@@ -1,1 +1,12 @@
 include_recipe "golang::default"
+
+execute "install go imports" do
+  command "go get code.google.com/p/go.tools/cmd/goimports"
+  action :run
+  user    node[:bootstrap][:user]
+  group   node[:bootstrap][:user]
+end
+
+link "/usr/local/bin/goimports" do
+  to "#{node['go']['gopath']}/bin/goimports"
+end
