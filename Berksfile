208a3dbfb94ae7dcf5739c19ef94527e45816be8
Jose Lopez <jose.lopez@sendgrid.com>
2015-06-01 14:02:46 -0700

    add golang to setup

--- Berksfile
+++ Berksfile
@@ -1,3 +1,4 @@
 cookbook 'docker', github: 'bflad/chef-docker'
+cookbook 'golang', github: 'NOX73/chef-golang'
 
 source 'https://api.berkshelf.com'
--- attributes/attrubutes.rb
+++ attributes/attrubutes.rb
@@ -2,3 +2,6 @@ default[:bootstrap][:user] = "#{ENV['USER']}"
 
 # default ubuntu distribution
 default[:bootstrap][:distribution] = "trusty"
+
+
+default[:go][:version] = "1.4"
